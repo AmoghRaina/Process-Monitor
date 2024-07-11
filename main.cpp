dacb184e7aa7b8f4aa40b9c385524ff4a3eae62f
AmoghRaina <amoghraina2000@gmail.com>
2024-07-12 01:14:11 +0530

    Logic changes

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.setWindowTitle("Process Finder");
+    w.setWindowTitle("Process-Monitor");
     w.setWindowIcon(QIcon(":/resources/rsimg/file.ico"));
     w.show();
     return a.exec();
